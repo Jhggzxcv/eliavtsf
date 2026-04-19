759d66f614f4809202b0ca4e13cd53daa7edd6ff
eliavtsf <zpnyhlyb@gmail.com>
2026-04-19 07:46:50 +0300

    fdgsxzvs

--- EliavTsfWebSite/manager.aspx
+++ EliavTsfWebSite/manager.aspx
@@ -3,5 +3,21 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+<h1>דף מנהל</h1>
+  
+    first name:
+   <input type="text" name="firstName" placeholder="example"/>
+    <br /><br/>
+    
+    last name:
+   <input type="text" name="lastName" placeholder="example"/>
+    <br /><br/>
+
+    <input type="submit" value="Search"/>
+    <br /><br/>
+    
+
+
 </asp:Content>
 
--- EliavTsfWebSite/manager.aspx.cs
+++ EliavTsfWebSite/manager.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +10,18 @@ public partial class manager : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        string firstname = Request.Form["firstName"];
+        string lastName = Request.Form["lastName"];
 
+        string sql =
+            "SELECT * FROM [dbo].[tUsers] " +
+            "WHERE FirstName LIKE N '%" + firstname + "%' " +
+            "AND LastName LIKE N'%" + lastName + "%'";
+
+        DataTable dt = MyAdoHelper.ExecuteDataTable(sql);
     }
 }
+
+
+       
+           
